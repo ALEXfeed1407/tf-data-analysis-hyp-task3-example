1b26ec03e9e22cc9603cc06b41de30c9e2a52f5b
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-11 18:11:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    from scipy.stats import ttest_1samp,t
+    alpha = 0.02
+    threshold = 500
+    mean = np.mean(historical_data)
+    std_dev = np.std(historical_data)
+    t_statistic, p_value = ttest_1samp(historical_data, threshold)
+    df = len(historical_data) - 1
+    t_critical = np.abs(t.ppf(alpha / 2, df))
+    des = t_statistic > t_critical
+    return des # Ваш ответ, True или False
