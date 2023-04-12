9b51928c26d9107f10fbda8941d15aa5a0fd1299
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-12 14:58:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,7 @@ import numpy as np
 chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool:
+    import scipy.stats as st
     alpha = 0.02
     threshold = 500
     t_statistic, p_value = st.ttest_1samp(x, threshold, alternative='less')
